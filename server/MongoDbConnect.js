a7355542da3bfe141198c07fec930d583910e68f
irene mmassy <info@zpunet.com>
2022-12-30 18:12:10 +0300

    updates

--- server/MongoDbConnect.js
+++ server/MongoDbConnect.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 export const MongodbConnect = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connect");
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
